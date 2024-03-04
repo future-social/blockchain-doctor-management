cc90dc44a996ddc19b87f58ff19ac13222dff000
白右熊 <81235502+future-social@users.noreply.github.com>
2024-03-04 15:34:51 +0800

    Update doctor-management-chaincode.go

--- chaincode/doctor-management-chaincode.go
+++ chaincode/doctor-management-chaincode.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 	"log"
 	"time"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 // DoctorContract to define the smart contract
