218dfaefb424bd7d250016cc738709d20494e636
白右熊 <81235502+future-social@users.noreply.github.com>
2024-03-06 19:14:26 +0800

    server path

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,18 @@ const routes = require('./route');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(bodyParser.json());
 app.use('/', routes);
 
 // Serve static files from the 'front' directory
-app.use(express.static(path.join(__dirname, '..', 'front')));
+// app.use(express.static(path.join(__dirname, '..', 'HTML')));
 
 // Serve index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'front','HTML', 'Admin_DoctorPersonalInformation01.html'));
+    res.sendFile(path.join(__dirname, '..','HTML', 'Admin_DoctorPersonalInformation01.html'));
 });
 
 // Start server
