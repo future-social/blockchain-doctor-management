13e81cfe699eb98caacb4f5456110eefcc1713f0
白右熊 <81235502+future-social@users.noreply.github.com>
2024-03-06 15:39:32 +0800

    fix(fea): fix register doctor feature

--- backend/registerDoctor.js
+++ backend/registerDoctor.js
@@ -34,11 +34,11 @@ async function registerDoctorUser() {
     const wallet = await Wallets.newFileSystemWallet(walletPath);
 
     // Check if the user identity already exists in the wallet
-    const doctorId = process.argv[2];
-    const userExists = await wallet.get(doctorId);
+    const DMSDoctorId = process.argv[2];
+    const userExists = await wallet.get(DMSDoctorId);
     if (userExists) {
       console.log(
-        `An identity for the user ${doctorId} already exists in the wallet`
+        `An identity for the user ${DMSDoctorId} already exists in the wallet`
       );
       return;
     }
@@ -53,17 +53,51 @@ async function registerDoctorUser() {
       return;
     }
 
-    // Register the user identity with the CA
+    // build a user object for authenticating with the CA
+    const provider = wallet
+      .getProviderRegistry()
+      .getProvider(adminIdentity.type);
+    const adminUser = await provider.getUserContext(adminIdentity, "admin");
+
+    const secret = await ca.register(
+      {
+        enrollmentID: DMSDoctorId,
+        affiliation: "org1.department1", // Specify the affiliation if necessary
+        role: "client",
+      },
+      adminUser
+    );
+    const enrollment = await ca.enroll({
+      enrollmentID: DMSDoctorId,
+      enrollmentSecret: secret,
+    });
+
+    const x509Identity = {
+      credentials: {
+        certificate: enrollment.certificate,
+        privateKey: enrollment.key.toBytes(),
+      },
+      mspId: "Org1MSP",
+      type: "X.509",
+    };
+
+    await wallet.put(DMSDoctorId, x509Identity);
+    console.log(
+      `Successfully registered and enrolled user ${DMSDoctorId} and imported it into the wallet`
+    );
+    /*
+    Register the user identity with the CA
     const registerRequest = {
-      enrollmentID: doctorId,
+      enrollmentID: DMSDoctorId,
       affiliation: "org1.department1", // Specify the affiliation if necessary
       role: "client", // Specify the role of the user (e.g., client, peer, orderer)
       attrs: [{ name: "DMSrole", value: "doctor", ecert: true }], // Additional attributes if needed
     };
     const enrollmentSecret = await ca.register(registerRequest, adminIdentity);
     console.log(
-      `Successfully registered user ${doctorId} with enrollment secret ${enrollmentSecret}`
-    );
+      `Successfully registered user ${DMSDoctorId} with enrollment secret ${enrollmentSecret}`
+    ); 
+    */
   } catch (error) {
     console.error(`Failed to register user: ${error}`);
     process.exit(1);
