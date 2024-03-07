2cd9db62ff46dc2d4af5d7e68f3f9fc9b3cacefb
白右熊 <81235502+future-social@users.noreply.github.com>
2024-03-07 15:17:09 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const routes = require('./route');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(bodyParser.json());
