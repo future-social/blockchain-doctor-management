ee801b383398a18476a0847eaa412635687c237b
nienyou <64187730+nienyou@users.noreply.github.com>
2024-03-10 17:53:16 +0800

    Add files via upload

--- backend/route.js
+++ backend/route.js
@@ -1,94 +1,89 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-// const doctorController = require('./doctorController');
-// const registerDoctor = require('./registerDoctor');
+const doctorController = require('./doctorController');
+const registerDoctor = require('./registerDoctor');
 const DMSAdminId = "DMSadmin10"; // TEST
 
 // Route to create a new doctor
-router.post("/createDoctor", async (req, res) => {
-  try {
-    console.log("Received doctor data:", doctorData);
-    const doctorData = req.body;
-    const result = await doctorController.createDoctor(doctorData, DMSAdminId);
-    registerDoctor.registerDoctorUser(doctorData["doctor_id"]);
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+router.post('/createDoctor', async (req, res) => {
+    try {
+        const doctorData = req.body;
+        const result = await doctorController.createDoctor(doctorData, DMSAdminId);
+        registerDoctor.registerDoctorUser(doctorData['doctor_id']); 
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 });
 
 // Route to retrieve all doctor data
-router.get("/retrieveAllDoctor", async (req, res) => {
-  try {
-    const result = await doctorController.retrieveAllDoctor(DMSAdminId);
-    // const result = [{
-    //     doctor_id: "DOC01",
-    //     first_name: "first name",
-    //     last_name: "last name",
-    //     gender: "male",
-    //     specialisation: "heart specialist"
-    // }, {
-    //     doctor_id: "DOC02",
-    //     first_name: "first name",
-    //     last_name: "last name",
-    //     gender: "male",
-    //     specialisation: "lung specialist"
-    // },{
-    //     doctor_id: "DOC03",
-    //     first_name: "first name",
-    //     last_name: "last name",
-    //     gender: "female",
-    //     specialisation: "skin specialist"
-    // }];
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+router.get('/retrieveAllDoctor', async (req, res) => {
+    try {
+        const result = await doctorController.retrieveAllDoctor(DMSAdminId);
+        // const result = [{
+        //     doctor_id: "DOC01",
+        //     first_name: "first name",
+        //     last_name: "last name",
+        //     gender: "male",
+        //     specialisation: "heart specialist"
+        // }, {
+        //     doctor_id: "DOC02",
+        //     first_name: "first name",
+        //     last_name: "last name",
+        //     gender: "male",
+        //     specialisation: "lung specialist"
+        // },{
+        //     doctor_id: "DOC03",
+        //     first_name: "first name",
+        //     last_name: "last name",
+        //     gender: "female",
+        //     specialisation: "skin specialist"
+        // }];
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 });
 
 // Route to retrieve doctor data by id
-router.get("/retrieveDoctor/:doctorId", async (req, res) => {
-  try {
-    const doctorId = req.params.doctorId;
-    const result = await doctorController.retrieveDoctor(doctorId);
-    // const result = {
-    //     doctor_id: doctorId,
-    //     first_name: "first name",
-    //     last_name: "last name",
-    //     gender: "male",
-    //     specialisation: "heart specialist"
-    // };
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+router.get('/retrieveDoctor/:doctorId', async (req, res) => {
+    try {
+        const doctorId = req.params.doctorId;
+        const result = await doctorController.retrieveDoctor(doctorId);
+        // const result = {
+        //     doctor_id: doctorId,
+        //     first_name: "first name",
+        //     last_name: "last name",
+        //     gender: "male",
+        //     specialisation: "heart specialist"
+        // };
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 });
 
 // Route to update doctor data
-router.put("/updateDoctor/:doctorId", async (req, res) => {
-  try {
-    const doctorId = req.params.doctorId;
-    const updatedData = req.body;
-    const result = await doctorController.updateDoctor(
-      DMSAdminId,
-      doctorId,
-      updatedData
-    );
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+router.put('/updateDoctor/:doctorId', async (req, res) => {
+    try {
+        const doctorId = req.params.doctorId;
+        const updatedData = req.body;
+        const result = await doctorController.updateDoctor(DMSAdminId, doctorId, updatedData);
+        res.json(result);
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 });
 
 // Route to delete doctor data
-router.delete("/deleteDoctor/:doctorId", async (req, res) => {
-  try {
-    const doctorId = req.params.appointmentId;
-    await doctorController.deleteDoctor(doctorId, DMSAdminId);
-    res.json({ success: true, message: "Doctor data deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+router.delete('/deleteDoctor/:doctorId', async (req, res) => {
+    try {
+        const doctorId = req.params.appointmentId;
+        await doctorController.deleteDoctor(doctorId, DMSAdminId);
+        res.json({ success: true, message: 'Doctor data deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 });
 
 // Route to create a new appointment
