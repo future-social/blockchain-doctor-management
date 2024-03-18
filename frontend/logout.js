dbaaa26a88ba546df0426ef967de8face16c5a9a
nienyou <64187730+nienyou@users.noreply.github.com>
2024-03-18 22:03:18 +0800

    Added append function

--- frontend/logout.js
+++ frontend/logout.js
@@ -15,3 +15,16 @@ function logout() {
     window.location.href = "LoginPage.html";
   }
 }
+
+function append(){
+    const user = localStorage.getItem('user');
+    const appointmentLink = document.getElementById('appointment');
+    const appUrl = `Doctor_Appointment01.html?doctorId=${user}`;
+
+    const infoLink = document.getElementById('info');
+    const infoUrl = `Doctor_PersonalInformation01.html?doctorId=${user}`;
+
+    appointmentLink.href = appUrl;
+    infoLink.href = infoUrl;
+}
+
