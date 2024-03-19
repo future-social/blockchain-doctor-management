3f27a67a3132789515c4ee103d05eb6cea18f1c4
nienyou <64187730+nienyou@users.noreply.github.com>
2024-03-19 15:35:23 +0800

    Added set doctor availability

--- backend/doctorController.js
+++ backend/doctorController.js
@@ -342,6 +342,56 @@ async function countDoctor(userId) {
   }
 }
 
+async function setDoctorAvailability(DMSAdminId) {
+  try {
+    // Load connection profile
+    const ccpPath = path.resolve(
+        __dirname,
+        "..",
+        "..",
+        "blockchain-doctor-management",
+        "test-network",
+        "organizations",
+        "peerOrganizations",
+        "org1.example.com",
+        "connection-org1.json"
+      );
+      const ccpJSON = fs.readFileSync(ccpPath, "utf8");
+      const ccp = JSON.parse(ccpJSON);
+      // Connect to the gateway
+      const walletPath = path.resolve(__dirname, "wallet");
+      const wallet = await Wallets.newFileSystemWallet(walletPath);
+      const identity = await wallet.get(DMSAdminId);
+      if (!identity) {
+        throw new Error(
+          "An identity for the user " +
+            DMSAdminId +
+            " does not exist in the wallet"
+        );
+      }
+    const gateway = new Gateway();
+    await gateway.connect(ccp, {
+      wallet,
+      identity: DMSAdminId,
+      discovery: { enabled: true, asLocalhost: true },
+    });
+    // Get the network and contract
+    const network = await gateway.getNetwork("medicpro");
+    const contract = network.getContract("basic");
+
+    await contract.submitTransaction(
+        "setDoctorAvailability",
+        DMSAdminId
+    );
+      console.log("Doctor availability set successfully!");
+  
+      // Disconnect from the gateway
+      await gateway.disconnect();
+  } catch (error) {
+    console.error(`Failed to set availability : ${error}`);
+  }
+}
+
 module.exports = {
   createDoctor,
   retrieveAllDoctor,
@@ -349,4 +399,5 @@ module.exports = {
   updateDoctor,
   deleteDoctor,
   countDoctor,
+  setDoctorAvailability
 };
